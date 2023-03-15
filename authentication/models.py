647e7d5e1d510d08ca4514454da1f805a715f212
James Muriuki <muriukijames33@gmail.com>
2023-03-15 20:58:20 +0300

    fixing git

--- authentication/models.py
+++ authentication/models.py
@@ -72,7 +72,7 @@ class User(PermissionsMixin,AbstractBaseUser,TrackingModel):
     )
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    user_uuid = models.UUIDField(default=uuid.uuid4, editable=False,unique=True)
+    # user_uuid = models.UUIDField(default=uuid.uuid4, editable=False,unique=True)
     email = models.EmailField(_("email address"), blank=False,unique=True)
     is_staff = models.BooleanField(
         _("staff status"),
